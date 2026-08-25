414330588a349a4a30211057f4520c1c8be88d01
ekrmabd <ekramhossainekram28@gmail.com>
2026-08-25 13:37:23 +0600

    change socket url

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -243,7 +243,7 @@
 
 
 
-      const socket = io("https://claysbd.xyz"); // Node server URL
+      const socket = io("https://theclaysbdapps.cloud"); // Node server URL
     let Admin_ID = "{{ user()->id }}";   // Logged in admin ID
 
     // Join admin room
